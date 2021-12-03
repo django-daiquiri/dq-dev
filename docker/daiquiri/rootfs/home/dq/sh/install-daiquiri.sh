4bb317244e2cf5dd8e5bf58850ad0fe8571b5bb8
olaf michaelis <o.mic@web.de>
2021-12-03 22:35:33 +0100

    Add auto_create_admin_user option

--- docker/daiquiri/rootfs/home/dq/sh/install-daiquiri.sh
+++ docker/daiquiri/rootfs/home/dq/sh/install-daiquiri.sh
@@ -10,9 +10,11 @@ if [[ $(pip3 freeze | grep -Poc "django-daiquiri") == "0" ]]; then
     python3 manage.py makemigrations
     python3 manage.py migrate
 
-    # silent because of the error message, that wp admin user already exists
-    # necessary to create the daiquiri admin user
-    python3 manage.py create_admin_user >/dev/null 2>&1
+    if [[ "$(echo ${AUTO_CREATE_ADMIN_USER} | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+        # silent because of the error message, that wp admin user already exists
+        # necessary to create the daiquiri admin user
+        python3 manage.py create_admin_user >/dev/null 2>&1
+    fi
 
     mkdir -p "${DQAPP}/vendor"
     python3 manage.py download_vendor_files

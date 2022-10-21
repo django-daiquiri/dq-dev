acaa2a23f6f5d1a3c11c0663027539783de20669
olaf michaelis <o.mic@web.de>
2022-10-21 11:39:21 +0200

    Add force reinstall option to conf.toml

--- docker/daiquiri/rootfs/home/dq/sh/install-app-requirements.sh
+++ docker/daiquiri/rootfs/home/dq/sh/install-app-requirements.sh
@@ -4,14 +4,20 @@ if [[ "${1}" == "--force" ]]; then
     export AUTO_PIP_INSTALL_APP_REQUIREMENTS="true"
 fi
 
+force_reinstall=""
+
+if [[ "$(echo ${PIP_FORCE_REINSTALL_APP_REQUIREMENTS} | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+    force_reinstall="--force-reinstall"
+fi
+
 if [[ "$(echo ${AUTO_PIP_INSTALL_APP_REQUIREMENTS} | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
     reqfile="${DQAPP}/requirements.txt"
     if [[ -f "${reqfile}" ]]; then
         echo "Install app requirements"
-        pip install -r ${reqfile} --force-reinstall
+        pip install -r ${reqfile} ${force_reinstall}
     else
         echo "cannot pip install, file does not exist: ${reqfile}"
     fi
 else
-    echo "auto pip install app requirements is disabled, use '--force' to do it anyway"
+    echo "auto pip install app requirements is disabled"
 fi

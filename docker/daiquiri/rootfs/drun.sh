62ce906fbf11c4003cd5dfbb1d258ec97431427e
kimakan <45099849+kimakan@users.noreply.github.com>
2024-07-19 12:40:19 +0200

    Overhaul installation of app requirements

--- docker/daiquiri/rootfs/drun.sh
+++ docker/daiquiri/rootfs/drun.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-${HOME}/sh/install-daiquiri.sh
 ${HOME}/sh/install-app-requirements.sh
+${HOME}/sh/install-daiquiri.sh
+
+if [[ "$(echo ${PIP_FORCE_REINSTALL_APP_REQUIREMENTS} | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+    ${HOME}/sh/install-app-requirements.sh --force
+fi
 
 python3 ${HOME}/py/render_supervisord_conf.py
 ${HOME}/bin/supervisord -c "${HOME}/conf/supervisord.conf"

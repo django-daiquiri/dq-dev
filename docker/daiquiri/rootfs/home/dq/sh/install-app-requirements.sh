a3c2680da16e0d3863042bdeedd53c924f10ff32
kimakan <45099849+kimakan@users.noreply.github.com>
2024-07-19 12:40:19 +0200

    Overhaul installation of app requirements

--- docker/daiquiri/rootfs/home/dq/sh/install-app-requirements.sh
+++ docker/daiquiri/rootfs/home/dq/sh/install-app-requirements.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-if [[ "${1}" == "--force" ]]; then
-    export AUTO_PIP_INSTALL_APP_REQUIREMENTS="true"
-fi
-
 force_reinstall=""
 
-if [[ "$(echo ${PIP_FORCE_REINSTALL_APP_REQUIREMENTS} | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
+if [[ "${1}" == "--force" ]]; then
     force_reinstall="--force-reinstall"
 fi
 
-if [[ "$(echo ${AUTO_PIP_INSTALL_APP_REQUIREMENTS} | tr '[:upper:]' '[:lower:]')" == "true" ]]; then
-    reqfile="${DQAPP}/requirements.txt"
-    if [[ -f "${reqfile}" ]]; then
-        echo "Install app requirements"
-        pip install -r ${reqfile} ${force_reinstall}
-    else
-        echo "cannot pip install, file does not exist: ${reqfile}"
-    fi
+reqfile="${DQAPP}/requirements.txt"
+if [[ -f "${reqfile}" ]]; then
+    echo "Install app requirements"
+    pip install -r ${reqfile} ${force_reinstall}
 else
-    echo "auto pip install app requirements is disabled"
+    echo "cannot pip install, file does not exist: ${reqfile}"
 fi

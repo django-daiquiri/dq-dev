e0042aa1c977d512114c71e566141573206e274f
Kirill Makan <kmakan@aip.de>
2026-01-07 11:40:36 +0100

    Add error message for files fixture if the module is not installed

--- docker/daiquiri/rootfs/home/dq/sh/add_files_path.sh
+++ docker/daiquiri/rootfs/home/dq/sh/add_files_path.sh
@@ -12,4 +12,8 @@ done
 
 conf="${HOME}/conf/fixture_files_path.json"
 
-python "${HOME}/app/manage.py" loaddata "${conf}"
+if python "${HOME}/app/manage.py" loaddata "${conf}" 2>/dev/null; then
+    echo "✔ Fixtures for the 'cms' path loaded successfully."
+else
+    echo "✖ Failed to load fixtures for the 'cms' path. (Possible reason: The 'files' app is deactivated)."
+fi
